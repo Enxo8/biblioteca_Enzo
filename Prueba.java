f6111b94053cb8d623086c612cf000eec83c6288
Enxo8 <enzox844@gmail.com>
2020-12-22 09:10:14 +0100

    Modificacion suma de enteros

--- Prueba.java
+++ Prueba.java
@@ -1,9 +1,25 @@
+import java.util.Scanner;
 
 public class Prueba {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		Scanner sc = new Scanner(System.in);
+
+		int num1;
+		int num2;
+		int total;
+
+		System.out.println("Programa que suma 2 numeros enteros introducidos por teclado");
+		System.out.println("Introduzca el primero");
+		num1 = Integer.parseInt(sc.next());
+		System.out.println("Introduzca el segundo");
+		num2 = Integer.parseInt(sc.next());
+		
+		total = num1 + num2;
+		
+		System.out.println("El resultado de la suma es: " + total);
+		
 	}
 
 }
